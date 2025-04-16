9a6a130ee487c00ab643a997a51abec091943648
Ziad Teama <ziadbasaria600@gmail.com>
2025-04-16 15:06:27 +0200

    .txt format

--- UI.cpp
+++ UI.cpp
@@ -4,41 +4,44 @@
 #include <iostream>
 using namespace std;
 
-int UI::getOperationMode() {
+int UI::getOperationMode()
+{
     int mode;
     cout << "Select mode: (0) Silent, (1) Interactive: ";
     cin >> mode;
     return mode;
 }
 
-string UI::getInputFileName() {
+string UI::getInputFileName()
+{
     string fileName;
     cout << "Enter input file name: ";
     cin >> fileName;
     return fileName;
 }
 
-string UI::getOutputFileName() {
+string UI::getOutputFileName()
+{
     string fileName;
     cout << "Enter output file name: ";
     cin >> fileName;
-    return fileName;
+    return fileName + ".txt";
 }
 
 void UI::printSystemStatus(
     int currentTime,
-    const LinkedQueue<Patient*>& allPatients,
-    const priQueue<Patient*>& inTreatment,
-    const ArrayStack<Patient*>& finishedPatients,
-    const LinkedQueue<Resource*>& eDevices,
-    const LinkedQueue<Resource*>& uDevices,
-    const LinkedQueue<Resource*>& xRooms,
-    const LinkedQueue<Patient*>& eWaiting,
-    const LinkedQueue<Patient*>& uWaiting,
-    const LinkedQueue<Patient*>& xWaiting,
-    const priQueue<Patient*>& latePatients,
-    const priQueue<Patient*>& earlyPatients
-) {
+    const LinkedQueue<Patient *> &allPatients,
+    const priQueue<Patient *> &inTreatment,
+    const ArrayStack<Patient *> &finishedPatients,
+    const LinkedQueue<Resource *> &eDevices,
+    const LinkedQueue<Resource *> &uDevices,
+    const LinkedQueue<Resource *> &xRooms,
+    const LinkedQueue<Patient *> &eWaiting,
+    const LinkedQueue<Patient *> &uWaiting,
+    const LinkedQueue<Patient *> &xWaiting,
+    const priQueue<Patient *> &latePatients,
+    const priQueue<Patient *> &earlyPatients)
+{
     cout << "\nCurrent Timestep: " << currentTime << "\n";
 
     // ALL Patients
@@ -48,32 +51,41 @@ void UI::printSystemStatus(
 
     // Waiting Lists
     cout << "============== Waiting Lists ==============\n";
-    cout << eWaiting.GetCount() << " Electrotherapy patients: "; eWaiting.print();
-    cout << uWaiting.GetCount() << " UltrasoundTherapy patients: "; uWaiting.print();
-    cout << xWaiting.GetCount() << " GymExercises patients: "; xWaiting.print();
+    cout << eWaiting.GetCount() << " Electrotherapy patients: ";
+    eWaiting.print();
+    cout << uWaiting.GetCount() << " UltrasoundTherapy patients: ";
+    uWaiting.print();
+    cout << xWaiting.GetCount() << " GymExercises patients: ";
+    xWaiting.print();
 
     // Early & Late Lists
     cout << "===============   Early List   ================\n";
-    cout << earlyPatients.GetCount() << " patients: "; earlyPatients.print();
+    cout << earlyPatients.GetCount() << " patients: ";
+    earlyPatients.print();
 
     cout << "===============   Late List   ================\n";
-    cout << latePatients.GetCount() << " patients: "; latePatients.print();
+    cout << latePatients.GetCount() << " patients: ";
+    latePatients.print();
 
     // Available Devices
     cout << "===============   Avail E-devices ===============\n";
-    cout << eDevices.GetCount() << " Electro devices: "; eDevices.print();
+    cout << eDevices.GetCount() << " Electro devices: ";
+    eDevices.print();
 
     cout << "===============   Avail U-devices ===============\n";
-    cout << uDevices.GetCount() << " Ultrasound devices: "; uDevices.print();
+    cout << uDevices.GetCount() << " Ultrasound devices: ";
+    uDevices.print();
 
     cout << "===============   Avail X-rooms ===============\n";
     xRooms.print();
 
     // In Treatment
     cout << "============  In-treatment List ================\n";
-    cout << inTreatment.GetCount() << " patients: "; inTreatment.print();
+    cout << inTreatment.GetCount() << " patients: ";
+    inTreatment.print();
 
     // Finished
     cout << "-------------------------------------------------------\n";
-    cout << finishedPatients.GetCount() << " Finished patients: "; finishedPatients.print();
+    cout << finishedPatients.GetCount() << " Finished patients: ";
+    finishedPatients.print();
 }
